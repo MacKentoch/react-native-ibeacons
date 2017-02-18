189855ef8230fe5bc37599122f0de83f93cbf011
MacKentoch <erwan.datin@gmail.com>
2017-02-18 12:07:19 +0100

    demo android wait message

--- examples/BeaconsDemo/index.android.js
+++ examples/BeaconsDemo/index.android.js
@@ -17,10 +17,10 @@ export default class BeaconsDemo extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Android iBeacon demo
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          Coming soon!!!
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
