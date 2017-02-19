3390a0428875948b842e17538a8798f635dc772d
MacKentoch <erwan.datin@gmail.com>
2017-02-19 05:56:53 +0100

    add android

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-import { NativeModules } from 'react-native';
-const NativeRNiBeacons = NativeModules.RNiBeacon;
+import { Platform } from 'react-native';
+import RNiBeaconAndroid from './lib.index.android';
+import RNiBeaconIOS from './lib.index.android';
 
-export default NativeRNiBeacons;
+function moduleSelector() {
+  if (Platform.OS === 'android') {
+      return RNiBeaconAndroid;
+  }
+  return RNiBeaconIOS;
+}
+
+const RNiBeaconsModule = moduleSelector();
+
+export default RNiBeaconsModule;
